11eeb26c5102e7d5d127b0c4647fe896cb8a24fa
oneRung <59422769+HanumanH@users.noreply.github.com>
2022-05-05 16:50:08 -0400

    Serving static file to client

--- server.js
+++ server.js
@@ -5,7 +5,13 @@
 // HTTP can perform server request such as GET,POST,PUT, or DELETE
  
 // import the HTTP module --> create a variable to store the returned HTTP instance
-http = require("http");
+const http = require("http");
+
+// import the FS module --> create a variable to store the returned FS instance
+const fs = require("fs");
+
+// import the PATH module --> create a variable to store the returned PATH instance
+const path = require("path");
 
 // create a variable for the IP address 127.0.0.1
 const hostname = "127.0.0.1";
@@ -21,9 +27,71 @@ const port = 3025;
 // the response object returns the data requested by the client
 
 // create a variable to hold the createServer() instance
- const server = http.createServer();
- server.on("request", (request, res) => {
-     res.end("your request has been served with this line of response");
+ const server = http.createServer(function (request, response) {
+    // logging the request URL and fixing it if it does not specify a file
+    // i.e., if the URL sent is paesino.com, it will be interpreted as paesino.com/index.html
+    console.log('request ', request.url); // print to console the file being requested by client
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './htmls/index.html';
+    }
+    // if request is /style.css, it will be interpreted as 
+    if (filePath == './style.css') {
+        filePath = './public/stylesheets/style.css';
+    }
+    // if request is /script.js, it will be interpreted as 
+    if (filePath == './script.js') {
+        filePath = './public/javascripts/script.js';
+    }
+    // if request is /loginPageBackground.jpg, it will be interpreted as 
+    if (filePath == './loginPageBackground.jpg') {
+        filePath = './images/loginPageBackground.jpg';
+    }
+    // change to string to lowercase --> look for the extension of the file being requested
+    // to see if it matches with one of our MIME types;
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+    // if no match found we use the application/octet-stream as the default type
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+    // respond to the client with the file information
+    // this function reads the file using our previously prepared filePath variable
+    fs.readFile(filePath, function(error, content) {
+        // compensate for any possible errors
+        if (error) {
+            // most often, the error will be ENOENT, in which case we reply with a 404 error
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(404, { 'Content-Type': 'text/html' });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        // if there are no errors, we send over the requested file
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
  });
 
 //  III. The Server is Listening
